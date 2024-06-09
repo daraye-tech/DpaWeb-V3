9c9d9554de1b650d3c40e96cbf5011f4b020622a
yaslam123 <yaslamahmed52@gmail.com>
2024-06-09 10:18:55 +0300

    Setup server with ES6 modules

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import next from 'next';
 
-const express = require('express');
-const next = require('next');
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const App = express();
+app.prepare().then(() => {
+    const server = express();
 
-const PORT = process.env.PORT || 3000;
+    server.all('*', (req, res) => {
+        return handle(req, res);
+    });
 
-App.listen(PORT, () => {
-    console.log(`running on PORT ${PORT}`);
+    const port = process.env.PORT || 3000;
+    server.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
+    });
 });
